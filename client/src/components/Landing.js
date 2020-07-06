3dcf0ce787595235c40b7563410aa41373478ebb
Alan Houston <alanhouston5000@gmail.com>
2020-07-06 16:53:44 -0500

    up to navbar

---
+++ client/src/components/Landing.js
@@ -0,0 +1,17 @@
+import react, {Component} from 'react';
+
+class Landing extends Component {
+    render() {
+        return (
+             <div className='container'>
+                 <div className='jumbptron mt-5'>
+                     <div className='col-sm-8 mx-auto'>
+                         <h1 className='text-center'>HOWDY</h1>
+                     </div>
+                 </div>
+             </div>
+        )
+    }
+}
+
+export default Landing;
