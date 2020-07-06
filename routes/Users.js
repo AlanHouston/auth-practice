c1b74cfc014c2d74126cfef06b30831677e19ece
Alan Houston <alanhouston5000@gmail.com>
2020-07-06 16:05:31 -0500

    login, react installed

--- routes/Users.js
+++ routes/Users.js
@@ -45,4 +45,31 @@ users.post('/register', (req,res) => {
     })
 });
 
+users.post('/login', (req,res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(user => {
+        if (user) {
+            console.log('1: ' + req.body.password);
+            console.log('2: ' + user.password);
+            if (bcrypt.compare(req.body.password, user.password)) {
+                let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
+                    expiresIn: 1440
+                })
+                res.send(token);
+            } else {
+                res.status(400).json({error: 'Incorrect password'});
+            }
+        } else {
+            res.status(400).json({error: 'User does not exist'});
+        }
+    })
+    .catch(err => {
+        res.status(400).json({error: err});
+    })
+})
+
 module.exports = users;
